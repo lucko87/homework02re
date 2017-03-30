030fc49571a0f836502ca27fad459f90497a5727
lucko87 <lucko87@gmail.com>
2017-03-30 15:28:22 +0200

    #1
    Kiírja a mainban megadott értékről, hogy páros-e vagy páratlan.

--- src/hu/basic/CalculatorService.java
+++ src/hu/basic/CalculatorService.java
@@ -1,7 +1,15 @@
 package hu.basic;
 
-/**
- * Created by István on 2017. 03. 30..
- */
+//1. feladat
+//Lérezhonom a CalculatorService osztályt
 public class CalculatorService {
+    //Azonosítom az oszályt, hogy később hivatkozni tudjak rá
+    public CalculatorService() {
+    }
+
+    //Létrehozom, az isIt Even metódust, ami publikus és azt mondom, hogy tartalamazzon egy Integer számot
+    public boolean isItEven(int number) {
+//Azt mondom, hogy a metódus visszatérési értéke a kettővel való oszts
+        return !(number % 2 != 0);
+    }
 }
--- src/hu/basic/Main.java
+++ src/hu/basic/Main.java
@@ -3,6 +3,14 @@ package hu.basic;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // 1. Feladat
+
+        //Kiírom, hogy melyik feladatról van szó, majd sortörlést használok a függvényben
+        System.out.println("1. feladat: A 6-os szám értéke páros, ez az állítás: \n");
+        // Behívom a CalculatorServic osztályt, és azt mondom, hogy új értéket akarok neki küldeni
+        CalculatorService calculatorService = new CalculatorService();
+        //Kiirítom a CalculatorServic osztály, isItEven metódusának eredményét, ha a küldött érték 6
+        System.out.println(calculatorService.isItEven(6) + "\n");
+
     }
 }
